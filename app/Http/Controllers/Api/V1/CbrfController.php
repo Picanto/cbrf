eede51a4623d334b285be362a3f9a656ac864f1e
vberestyansky <vberestyansky@ves-media.com>
2023-04-20 21:35:49 +0300

    настройка маршрутизации

--- app/Http/Controllers/Api/V1/CbrfController.php
+++ app/Http/Controllers/Api/V1/CbrfController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cbrf;
 use Illuminate\Http\Request;
 
 class CbrfController extends Controller
 {
     //
+    public function index() {
+        return Cbrf::all();
+    }
 }

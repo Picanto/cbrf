bd7e84d559defbf5465fc0312182fdf10b073e18
vberestyansky <vberestyansky@ves-media.com>
2023-04-21 20:59:01 +0300

    тест

--- database/migrations/2023_04_21_160753_create_valutes_table.php
+++ database/migrations/2023_04_21_160753_create_valutes_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('charcode');
             $table->string('value');
             $table->date('date');
+            $table->string('uid');
             // $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
             // $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });

37f0dacfe31361b0e16305f0c62856b4912dc485
vberestyansky <vberestyansky@ves-media.com>
2023-04-21 18:57:42 +0300

    тест

--- app/cbr/cbr.php
+++ app/cbr/cbr.php
@@ -16,19 +16,21 @@ class cbr {
     }
 
     public function getCbrXml() {
+
         $full_url = $this->cbr_xml . $this->getDate();
         $xml_string = file_get_contents($full_url);
         return $xml_string;
+
     }
 
     public function xmlToJson($xml_string) {
+
         $xml = simplexml_load_string($xml_string);
         $json = json_encode($xml);
         $json = json_decode($json,TRUE);
 
         return $json['Valute'];
 
-        // return $json['Valute'][0]['CharCode'] . $json['Valute'][0]['Name'] . $json['Valute'][0]['Value'];
     }
 
     public function getValute($name, $array) {

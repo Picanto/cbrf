b7944b7d0048328278627c7bc03b9dcf6c704acb
vberestyansky <vberestyansky@ves-media.com>
2023-04-21 21:01:28 +0300

    создание новой миграции Valutes

---
+++ app/Models/Valutes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Valutes extends Model
+{
+    use HasFactory;
+}

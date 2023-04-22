f0b42ce3d8c0e2ba74ab5879eda5fdd7c03ae0e0
vberestyansky <vberestyansky@ves-media.com>
2023-04-22 22:24:06 +0300

    рефакторинг, переработка методов

--- app/Http/Controllers/Api/V1/CbrfController.php
+++ app/Http/Controllers/Api/V1/CbrfController.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 
 class CbrfController extends Controller
 {
+
     public function index(Request $request) {
 
         $get_params = $request->query();
@@ -25,14 +26,134 @@ class CbrfController extends Controller
         $comment = $cbrDto->getComment();
 
         $cbr = new cbr($date);
+
+        // Если валюта указана (запрос для одной валюты)
+        if(!is_null($valute)) {
+
+            $valuteFromDataBase = $cbr->checkInDataBase_2($date, $valute);
+
+//            var_dump($valuteFromDataBase);
+//            exit();
+
+            // Проверить наличие валют в таблице для минимизации запросов ко внешнему сервису
+            if ($valuteFromDataBase) {
+                return $valuteFromDataBase;
+            } else {
+                // Если не найдено в таблице, то загрузить с сайта
+                $xml = $cbr->getCbrXml();
+                $all_valute_array = $cbr->xmlToJson($xml);
+
+                $valuteArray = $cbr->getValute($valute, $all_valute_array, $comment);
+
+                if($save === 'true') {
+                    $cbr->saveToDataBase($valuteArray);
+                }
+
+                return $valuteArray;
+            }
+
+        } else {
+            // Иначе, если валюта не указана, то запрос ко всем валютам
+//            var_dump('все валюты');
+//            exit();
+
+            $valuteFromDataBase = $cbr->checkInDataBase_2($date, $valute);
+
+            // var_dump($valuteFromDataBase);
+
+            // var_dump($valuteFromDataBase[14][0]['charcode']);
+
+            $charCodesFromDataBase = [];
+
+            // Список валют, которые есть в базе
+            for($i = 0; $i < count($valuteFromDataBase); $i++) {
+                // if(array_key_exists('charcode', $valuteFromDataBase)) {
+                    // var_dump($valuteFromDataBase[$i][0]['charcode']);
+                $charCodesFromDataBase[] = $valuteFromDataBase[$i][0]['charcode'];
+                // }
+
+            }
+
+            //--------------------------------------------
+//            // Если не найдено в таблице, то загрузить с сайта
+//            $xml = $cbr->getCbrXml();
+//            $all_valute_array = $cbr->xmlToJson($xml);
+//
+//            $valuteArray = $cbr->getValute($valute, $all_valute_array, $comment);
+//
+//            var_dump($valuteArray);
+//            exit();
+//
+//            if($save === 'true') {
+//                // $cbr->saveToDataBase($valuteArray);
+//            }
+//
+//            return $valuteArray;
+            //--------------------------------------------
+
+            $allCharCodes = $cbr->getAllCodes();
+
+            // var_dump($charCodesFromDataBase);
+
+            $arrayDiff = array_values(array_diff($allCharCodes, $charCodesFromDataBase));
+
+//            var_dump($arrayDiff);
+//            exit();
+
+            $valuteArray = [];
+
+            for($i = 0; $i < count($arrayDiff); $i++) {
+
+                // Если не найдено в таблице, то загрузить с сайта
+                $xml = $cbr->getCbrXml();
+                $all_valute_array = $cbr->xmlToJson($xml);
+
+                $valuteArray[] = $cbr->getValute($arrayDiff[$i], $all_valute_array, $comment);
+
+            }
+
+//            var_dump(count($valuteArray));
+//            exit();
+
+            // for($i = 0; $i < count($valuteArray); $i++) {
+                if($save === 'true') {
+                    $cbr->saveToDataBase($valuteArray);
+                    // var_dump($valuteArray[$i][0]);
+                }
+            // }
+
+//            var_dump($valuteArray[0][0]);
+//            exit();
+
+            // if(empty($valuteArray)) {
+                return $cbr->checkInDataBase_2($date, $valute);
+            // }
+
+            // return $valuteArray;
+
+//            exit();
+
+//            var_dump($valuteFromDataBase);
+//            exit();
+
+        }
+
+        // $xml = $cbr->getCbrXml();
+        // $all_valute_array = $cbr->xmlToJson($xml);
+        // var_dump($xml);
+//        exit();
+
+        /*
         $xml = $cbr->getCbrXml();
 
         $all_valute_array = $cbr->xmlToJson($xml);
 
+        // массив уникальных id, состоящий из хэш-функции md5 (date+CharCode)
+        $uids_array = [];
+
         if($save === 'true') {
 
             foreach($cbr->getValute($valute, $all_valute_array) as $item) {
-
 
                 $valutes = new Valutes();
                 // echo $item['CharCode'] . ' ' . $item['Value'];
@@ -42,10 +163,28 @@ class CbrfController extends Controller
                 $valutes->uid = md5($date.$item['CharCode']);
                 $valutes->comment = $comment;
                 // return $valutes->getTable();
-                $valutes->save();
+                // $valutes->save();
+
+                $uids_array[] = md5($date.$item['CharCode']);
+
+            }
+
+        } else {
+
+            foreach($cbr->getValute($valute, $all_valute_array) as $item) {
+                $uids_array[] = md5($date.$item['CharCode']);
             }
 
         }
 
+        return $cbr->checkInDataBase($uids_array);
+
+        // var_dump($cbr->checkInDataBase($uids_array));
+        // exit();
+
+//        return $uids_array;
+        */
+
     }
+
 }
--- app/cbr/cbr.php
+++ app/cbr/cbr.php
@@ -2,6 +2,11 @@
 
 namespace App\cbr;
 
+use App\Models\Cbrf;
+use App\Models\Valutes;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
 class cbr {
 
     private $cbr_xml = 'https://www.cbr.ru/scripts/XML_daily.asp?date_req=';
@@ -11,10 +16,26 @@ class cbr {
         $this->date = $date;
     }
 
+    /**
+     * @return string[]
+     */
+    public function getAllCodes(): array {
+        $array = ['AUD','AZN','GBP','AMD','BYN','BGN','BRL','HUF','VND','HKD','GEL','DKK','AED','USD','EUR','EGP',
+            'INR','IDR','KZT','CAD','QAR','KGS','CNY','MDL','NZD','NOK','PLN','RON','XDR','SGD','TJS','THB','TRY',
+            'TMT','UZS','UAH','CZK','SEK','CHF','RSD','ZAR','KRW','JPY'];
+        return $array;
+    }
+
+    /**
+     * @return mixed
+     */
     private function getDate() {
         return $this->date;
     }
 
+    /**
+     * @return false|string
+     */
     public function getCbrXml() {
 
         $full_url = $this->cbr_xml . $this->getDate();
@@ -23,6 +44,10 @@ class cbr {
 
     }
 
+    /**
+     * @param $xml_string
+     * @return mixed
+     */
     public function xmlToJson($xml_string) {
 
         $xml = simplexml_load_string($xml_string);
@@ -33,17 +58,23 @@ class cbr {
 
     }
 
-    public function getValute($name, $array) {
+    /**
+     * @param $name
+     * @param $array
+     * @return array
+     */
+    public function getValute($name, $array, $comment) {
 
         $all_valutes = [];
 
-        if(is_null($name) || $name === 'all') {
+        if(is_null($name)) {
 
             foreach($array as $item) {
                 $all_valutes[] = ['CharCode' => $item['CharCode'],
                                   'Name'  => $item['Name'],
                                   'Value' => $item['Value'],
-                                  'Date'  => $this->date
+                                  'Date'  => $this->date,
+                                  'Comment' => $comment
                 ];
             }
 
@@ -55,13 +86,150 @@ class cbr {
                 $all_valutes[] = ['CharCode' => $item['CharCode'],
                                   'Name'  => $item['Name'],
                                   'Value' => $item['Value'],
-                                  'Date'  => $this->date
+                                  'Date'  => $this->date,
+                                  'Comment' => $comment
                 ];
             }
 
         }
 
         return $all_valutes;
+
+    }
+
+//    public function checkInDataBase(array $hashIdsArray) {
+//
+//        $uids = DB::table('valutes')->select('uid')->get();
+//
+//        $uidsDbArray = [];
+//
+//        foreach($uids as $item) {
+//            $uidsDbArray[] = $item->uid;
+//        }
+//
+////        var_dump($hashIdsArray);
+////        var_dump($uidsDbArray);
+////        exit();
+//
+//        // $arrayDiff = array_diff($hashIdsArray, $uidsDbArray);
+//
+//        // if(empty($arrayDiff)) {
+//            // $this->getDataFromDataBase($hashIdsArray);
+////            var_dump($this->getDataFromDataBase($hashIdsArray));
+////            exit();
+//        // }
+//
+//        return $this->getDataFromDataBase($hashIdsArray);
+//
+////        return $arrayDiff;
+//
+//    }
+
+    /**
+     * @param $array
+     * @return array
+     */
+    private function getDataFromDataBase($array): array {
+
+        // $records = DB::table('valutes')->where('uid', '038523354f2e28369d4782726da3b9b9');
+
+        // $records = DB::table('valutes')->select('charcode', 'value', 'date', 'uid', 'comment')->get();
+
+//        var_dump($array);
+//        exit();
+
+        $records = DB::table('valutes')
+                    ->whereIn('uid', $array)
+                    ->get();
+
+//        var_dump($records);
+//        exit();
+
+        $resultArray = [];
+
+        foreach($records as $item) {
+
+            $resultArray[] = ['charcode' => $item->charcode,
+                            'value' => $item->value,
+                            'date' => $item->date,
+                            'uid' => $item->uid,
+                            'comment' => $item->comment
+            ];
+
+        }
+
+        return $resultArray;
+
+    }
+
+    /**
+     * @param $date
+     * @param $valute
+     * @return array
+     */
+    public function checkInDataBase_2($date, $valute): array {
+
+        if(is_null($valute)) {
+            $allCodesArray = $this->getAllCodes();
+
+            $valutesFromDataBase = [];
+
+            for($i = 0; $i < count($allCodesArray); $i++) {
+                $uid = md5($date.$allCodesArray[$i]);
+                // var_dump($this->getDataFromDataBase([$uid]));
+
+                $dataFromDataBase = $this->getDataFromDataBase([$uid]);
+
+                if(!empty($dataFromDataBase)) {
+                    $valutesFromDataBase[] = $dataFromDataBase;
+                }
+
+                // $valutesFromDataBase[] = $this->getDataFromDataBase([$uid]);
+
+            }
+
+            return $valutesFromDataBase;
+
+//            exit();
+        }
+
+//        var_dump($date);
+//        var_dump($valute);
+//        var_dump(md5($date.$valute));
+//        exit();
+
+        $uid = md5($date.$valute);
+        return $this->getDataFromDataBase([$uid]);
+
+//        var_dump($this->getDataFromDataBase([$uid]));
+//        exit();
+
+    }
+
+    public function saveToDataBase($valuteArray) {
+
+//        var_dump($valuteArray['CharCode']);
+//        exit();
+
+        foreach($valuteArray as $item) {
+
+//            var_dump($item[0]['CharCode']);
+
+            $valutes = new Valutes();
+            // echo $item['CharCode'] . ' ' . $item['Value'];
+            $valutes->charcode = $item[0]['CharCode'];
+            $valutes->value = $item[0]['Value'];
+            $valutes->date = Carbon::createFromFormat('d/m/Y', $item[0]['Date']);
+            $valutes->uid = md5($item[0]['Date'].$item[0]['CharCode']);
+            $valutes->comment = $item[0]['Comment'];
+            // return $valutes->getTable();
+             $valutes->save();
+
+            // $uids_array[] = md5($date.$item['CharCode']);
+
+        }
+
+//        exit();
 
     }
 
